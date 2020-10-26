26f5c06f96d60cda70f67255c36a3a62672ca5e5
bgloh <byoungloh@gmail.com>
2020-10-26 22:41:37 +0900

    Create pymata_ultrasonic_ping_2ch.py

---
+++ python/pymata_ultrasonic_ping_2ch.py
@@ -0,0 +1,20 @@
+# pymata_ultrasonc_ping_2ch.py
+# 2 ch input : trig, echo
+# FirmataPlus is required on Arduino
+
+import time
+from PyMata.pymata import PyMata
+
+PORT = '/dev/ttyACM0'
+board = PyMata(PORT,verbose=True)
+trig=8
+echo=9
+board.sonar_config(trig,echo)
+time.sleep(1)
+
+
+while True:
+  data = board.get_sonar_data()
+  dist = data[trig]
+  print 'distance: {} cm'.format(dist[1])
+  time.sleep(.1)
