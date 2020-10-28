866ab8023e444663f5207026bca5b15281d27c92
bgloh <byoungloh@gmail.com>
2020-10-28 22:24:28 +0900

    Create pymata_digitalIO.py

---
+++ python/pymata_digitalIO.py
@@ -0,0 +1,24 @@
+# pymata_digitalIO.py
+# PyMata digital out example
+from PyMata.pymata import PyMata
+import time
+
+# Pin no.
+pinNo = 8
+# connection port
+PORT = '/dev/ttyACM0'
+# Create PyMata instance
+board = PyMata(PORT, verbose=True)
+
+# Set digital pin 8 to be  an output port
+board.set_pin_mode(pinNo,board.OUTPUT,board.DIGITAL)
+
+# Blink for 10 times
+for x in range(10):
+  board.digital_write(pinNo,1)
+  time.sleep(.5)
+  board.digital_write(pinNo,0)
+  time.sleep(.5)
+
+# Close PyMata
+board.close()
